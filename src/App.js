74a167c7f673bdd203c5414c6bcd563180ff6a84
michal.zfania <michal.zfania@imperva.com>
2021-07-01 04:31:03 +0300

    temp init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {configureStore} from 'redux-toolkit';
 
-function App() {
+
+const App = () => {
+  const store = configureStore();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+          <WeatherTable></WeatherTable>
+      </Provider>
   );
 }
 
